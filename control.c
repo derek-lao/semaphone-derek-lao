08b690c35133807b3b685b7243a2e140349f0a84
derek-lao <dlao00@stuy.edu>
2020-01-06 05:21:19 -0500

    wrote write function, have yet to test

--- control.c
+++ control.c
@@ -10,15 +10,20 @@ void create()
 	shmid = shmget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
 	semid = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
 	fileDescriptor = semget("story.txt", O_RDWR | O_CREAT | O_TRUNC , 0666);
-//still need to set value of semaphore
+	semctl(semid, 1, SETVAL);//only one person can access the story at a time
 }
 
 void remove(int shmid, int semid, int fileDescriptor)
 {
+	int semval = shmctl(shmid, GETVAL, 0);
+	while(!semval)
+	{//do nothing
+	}
 	shmctl(shmid, IPC_RMID, 0);
-	semctl(semid, IPC_RMID, 0);
-//still need to display contents
-//still need to wait for until semaphore is available
+	semctl(semid, 0, IPC_RMID);
+	char story[10000];
+	read(fileDescriptor, story, 10000);
+	write(stdout, story, 10000);
 }
 
 void view(int fileDescriptor)
--- write.c
+++ write.c
@@ -1,1 +1,20 @@
 #include "functions.h"
+
+void write(int semid, int fileDescriptor)
+{
+	printf("Last addition: ");
+	read(fileDescriptor);
+	semval = semctl(semid, 0, GETVAL); 
+	while(!semval)
+	{//do nothing
+	}
+	semctl(semid, 0, SETVAL, semval - 1);//down the semaphore
+	semval --;
+	printf("\nYour addition: );
+	char story[10000];
+	read(stdin, story, 10000);
+	write(fileDescriptor, story, 10000);
+	write(stdout, story, 10000);
+	semctl(semid, 0, SETVAL, semval + 1);//up the semaphore
+	//semval++;
+}
