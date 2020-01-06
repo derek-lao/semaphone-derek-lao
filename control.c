0888a79ead87b88b52757550b7214697c5ac2d51
derek-lao <dlao00@stuy.edu>
2020-01-06 03:38:19 -0500

    finished writing code for view command

--- control.c
+++ control.c
@@ -5,9 +5,25 @@
 void create()
 {
 	int shmid;
-	int semaid;
+	int semid;
 	int fileDescriptor;
 	shmid = shmget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
 	semid = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
+	fileDescriptor = semget("story.txt", O_RDWR | O_CREAT | O_TRUNC , 0666);
+//still need to set value of semaphore
+}
 
+void remove(int shmid, int semid, int fileDescriptor)
+{
+	shmctl(shmid, IPC_RMID, 0);
+	semctl(semid, IPC_RMID, 0);
+//still need to display contents
+//still need to wait for until semaphore is available
+}
+
+void view(int fileDescriptor)
+{
+	char story[10000];
+	read(fileDescriptor, story, 10000);
+	write(stdout, story, 10000);
 }
