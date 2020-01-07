61358238ed5ed1a2e930eda79bd2dd621d354d1d
derek-lao <dlao00@stuy.edu>
2020-01-07 07:37:27 -0500

    fixed more compile-time errors, but not done yet

--- write.c
+++ write.c
@@ -4,13 +4,13 @@ void writeStory(int semid, int fileDescriptor)
 {
 	printf("Last addition: ");
 	readStory(fileDescriptor);
-	semval = semctl(semid, 0, GETVAL); 
+	int semval = semctl(semid, 0, GETVAL); 
 	while(!semval)
 	{//do nothing
 	}
 	semctl(semid, 0, SETVAL, semval - 1);//down the semaphore
 	semval --;
-	printf("\nYour addition: );
+	printf("\nYour addition:");
 	char story[10000];
 	read(stdin, story, 10000);
 	write(fileDescriptor, story, 10000);
