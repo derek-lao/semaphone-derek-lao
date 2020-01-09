df00e9b33e7a109825743dd51dd3068418edec98
derek-lao <43069580+derek-lao@users.noreply.github.com>
2020-01-09 00:00:10 -0500

    Update write.c
    deleted code that is already in functions.c

--- write.c
+++ write.c
@@ -4,22 +4,3 @@ int main()
 {
 	return 0;
 }
-
-void writeStory(int semid, int fileDescriptor)
-{
-	printf("Last addition: ");
-	viewStory(fileDescriptor);
-	int semval = semctl(semid, 0, GETVAL); 
-	while(!semval)
-	{//do nothing
-	}
-	semctl(semid, 0, SETVAL, semval - 1);//down the semaphore
-	semval --;
-	printf("\nYour addition:");
-	char story[10000];
-	read(stdin, story, 10000);
-	write(fileDescriptor, story, 10000);
-	write(stdout, story, 10000);
-	semctl(semid, 0, SETVAL, semval + 1);//up the semaphore
-	semval++;
-}
